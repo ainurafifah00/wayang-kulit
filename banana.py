6388ba36b369541562791238aafa25c6687314b6
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2020-11-20 11:54:08 +0800

    Added list of Git Commands for future reference

--- banana.py
+++ banana.py
@@ -3,3 +3,22 @@ for i in range(5):
 
 for i in range(3):
 	print('oranges')
+
+
+#Git Commands
+
+# git init
+# git add . / git add filename
+# git commit -m "Your message goes here"
+# git status
+# git log (hit 'q' to exit)
+#git checkout (unique commit id)
+
+
+#Git Remote Commands
+
+# git remote add origin (url)
+# git remote -v
+# git push origin master
+# git pull origin master
+# git clone (url)
